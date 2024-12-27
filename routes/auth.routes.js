0142ac7be7732b708eec7a41893efa327edfd31e
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2024-12-27 20:35:26 +0530

    added login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,22 +1,10 @@
-// const express = require('express');
-// const router = express.Router();
-
-// router.post('/signup', authController.signup);
-// router.post('/signin', authController.signin);
-// router.post('/signout', identifier, authController.signout);
-
-
-// module.exports = router;
-
-
-
 const router = require('express').Router();
 
-// const auth = require('../controllers/auth.controller');
+const auth = require('../controllers/auth.controller');
 // const { resetPassword, forgotPassword, login } = require('../validations/auth.validation');
 
 module.exports = (app) => {
-  // router.post('/login', login, auth.login);
+  router.post('/login', auth.login);
   // router.post('/forget-password', forgotPassword, auth.forgetPassword);
   // router.post('/reset-password', resetPassword, auth.resetPassword);
 
