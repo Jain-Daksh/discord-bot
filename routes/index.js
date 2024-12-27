bd0a0d58f318b278464429105919aa602b10efa7
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2024-12-27 20:36:21 +0530

    added routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 require('./user.routes')(router);
-// require('./auth.routes')(router);
-
+require('./subscription.routes')(router);
+require('./auth.routes')(router);
 
 module.exports.router = router;
