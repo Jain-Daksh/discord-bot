40304fb0ec881ec0c6114e9c37432a41ffb3caa7
Jain-Daksh <daksh@thedevelopercompany.com>
2024-12-27 12:01:05 +0530

    added env

--- discordBot.js
+++ discordBot.js
@@ -1,19 +1,23 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-client.once(Events.ClientReady, readyClient => {
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 client.on('messageCreate', (message) => {
-  console.log(message.content, 'message')
-})
+  console.log(message.content, 'message');
+});
 client.on('messageCreate', (message) => {
   if (message.author.bot) return null;
   message.reply({
     content: 'hi from bot',
-
-  })
-})
-
+  });
+});
 
 // Log in to Discord with your client's token
-client.login(process.env.tokenken);
+client.login(process.env.token);
