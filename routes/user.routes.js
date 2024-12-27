3974f443506dd5564423a6121233003144cfcd48
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2024-12-27 22:38:10 +0530

    added username get

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,8 +5,9 @@ const { isAuthenticated } = require('../utils/jwt');
 module.exports = (app) => {
   router.get('/', isAuthenticated, User.getAll);
   router.post('/', User.create);
+  // router.get('/:id', isAuthenticated, User.get);
 
-  router.get('/:id', isAuthenticated, User.get);
+  router.get('/:username', User.get);
   router.put('/:id', isAuthenticated, User.update);
   router.delete('/:id', isAuthenticated, User.delete);
 
