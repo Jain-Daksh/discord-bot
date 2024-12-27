002d165fa757acf2e8dc3822515a2686f927ecfb
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2024-12-27 22:37:49 +0530

    added command for subscription

--- discordBot/command.js
+++ discordBot/command.js
@@ -1,36 +1,53 @@
-const { Routes, REST } = require('discord.js')
+const { Routes, REST } = require('discord.js');
 
 const registerCommands = async (clientId, token) => {
-  console.log('token', clientId, token)
-  const commands = [{
-    name: 'ping',
-    description: "Replies with pong"
-  },
-    // {
-    //   name: 'createUser',
-    //   description: "create a new user"
-    // }
-  ]
+  const commands = [
+    {
+      name: 'ping',
+      description: 'Replies with pong',
+    },
+    {
+      name: 'createuser',
+      description: 'Create a new user',
+      options: [
+        {
+          type: 3,
+          name: 'username',
+          description: 'Enter the username',
+          required: true,
+        },
+        {
+          type: 3,
+          name: 'email',
+          description: 'Enter the email address',
+          required: true,
+        },
 
-  const rest = new REST({
-    version: '10'
-  }).setToken(
-    process.env.token
-  )
+      ],
+    },
+    {
+      name: 'ppgetuser',
+      description: 'Get user data by userID',
+      options: [
+        {
+          type: 3,
+          name: 'username',
+          description: 'Enter the user ID (username)',
+          required: true,
+        },
+      ],
+    },
+  ];
 
+  const rest = new REST({ version: '10' }).setToken(token);
 
   try {
-    console.log("Registering commands...");
+    console.log('Registering commands...');
     await rest.put(Routes.applicationCommands(clientId), { body: commands });
-    console.log("Commands registered successfully!");
+    console.log('Commands registered successfully!');
   } catch (error) {
     console.error('Error registering commands:', error);
   }
-
-}
-
-
-
-
+};
 
 module.exports = { registerCommands };
